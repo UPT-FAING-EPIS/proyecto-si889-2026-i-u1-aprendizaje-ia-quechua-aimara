b0581f5ae78d783220a155935c614474401c64aa
NestorSnIbz <nestorsnibz@gmail.com>
2026-05-28 16:09:26 -0500

    Release 1.0.1

--- android-app-v2/app/src/main/java/com/nescore/aprendizaje_ia_quechua_aimara/domain/model/User.kt
+++ android-app-v2/app/src/main/java/com/nescore/aprendizaje_ia_quechua_aimara/domain/model/User.kt
@@ -1,5 +1,8 @@
 package com.nescore.aprendizaje_ia_quechua_aimara.domain.model
 
+import androidx.annotation.Keep
+
+@Keep
 data class User(
     val uid: String,
     val email: String?,
--- android-app-v2/app/src/main/java/com/nescore/aprendizaje_ia_quechua_aimara/domain/model/WordleLanguage.kt
+++ android-app-v2/app/src/main/java/com/nescore/aprendizaje_ia_quechua_aimara/domain/model/WordleLanguage.kt
@@ -1,5 +1,8 @@
 package com.nescore.aprendizaje_ia_quechua_aimara.domain.model
 
+import androidx.annotation.Keep
+
+@Keep
 enum class WordleLanguage {
     QUECHUA,
     AIMARA
